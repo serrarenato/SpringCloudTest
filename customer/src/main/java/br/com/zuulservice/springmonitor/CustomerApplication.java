d121706358b603aa60e39ba6322e3c54a37bf59e
Renato <serra.renato@gmail.com>
2019-04-01 23:17:34 -0300

    Spring Monitor

--- customer/src/main/java/br/com/zuulservice/springmonitor/CustomerApplication.java
+++ customer/src/main/java/br/com/zuulservice/springmonitor/CustomerApplication.java
@@ -1,4 +1,4 @@
-package br.com.zuulservice.customer;
+package br.com.zuulservice.springmonitor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
--- customer/src/main/java/br/com/zuulservice/springmonitor/controller/CustomerController.java
+++ customer/src/main/java/br/com/zuulservice/springmonitor/controller/CustomerController.java
@@ -1,4 +1,4 @@
-package br.com.zuulservice.customer.controller;
+package br.com.zuulservice.springmonitor.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
